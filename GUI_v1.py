1506b1307f8698cc2d628447342656b4ba3eacdf
YuaHyodo <66828980+YuaHyodo@users.noreply.github.com>
2022-08-12 09:11:08 +0900

    Add files via upload

--- GUI_v1.py
+++ GUI_v1.py
@@ -3,39 +3,45 @@ from tkinter import*
 
 class Simple_GUI:
     def __init__(self):
+        #色とかを設定する
         self.area_A_bg = 'blue'
         self.area_A_button_color = ['gray', 'black']
         self.info_area1_bg_color = ['black', 'darkblue', 'yellow', 'purple']
         self.info_area1_fg_color = ['gold', 'black', 'yellow']
         self.info_area2_bg = 'skyblue'
         self.graph_color = 'orange'
+        #windowを初期化する
         self.init_window()
 
     def init_window(self):
+        #タイトル・サイズ等を決める
         self.title = 'Azisai_Othello_GUI(v1)'
         self.size = '500x500'
+        #windowを作り、サイズ・タイトルを設定する
         self.window = Tk()
         self.window.title(self.title)
         self.window.geometry(self.size)
 
+        #windowを4つのエリアに区切る
+        #それぞれのエリアに対応するFrameを作る
         self.area_A = Frame(self.window)#, width=500, height=20, bg=self.area_A_bg)
         self.Board_area = Frame(self.window)#, width=300, height=300)
         self.info_area1 = Frame(self.window)#, width=200, height=300)
         self.info_area2 = Frame(self.window)#, width=300, height=280)
 
+        #各種エリアをリセットする
         self.init_area_A()
         self.init_Board_area()
         self.init_info_area1()
         self.init_info_area2()
-        """
-        self.area_A.grid(column=0, row=0)
-        self.Board_area.grid(column=0, row=1)
-        self.info_area1.grid(column=1, row=1)
-        """
+
+        #サイズを決めて、配置する
         self.area_A.place(x=0, y=0, width=500, height=30)
         self.Board_area.place(x=0, y=30, width=320, height=320)
         self.info_area1.place(x=320, y=30, width=180, height=320)
         self.info_area2.place(x=0, y=350, width=500, height=150)
+
+        #更新を反映する
         self.window.update()
         return
 
@@ -43,32 +49,40 @@ class Simple_GUI:
         """
         上にあるやつ
         """
+        #それぞれのButtonのサイズを決める
         W = int(500 / 4)
         H = 30
+
+        #接続・対局をするButton
         self.online_button = Button(self.area_A,
                                         text='connect',
                                         bg=self.area_A_button_color[0],
                                         fg=self.area_A_button_color[1],
                                         command=self.online_play)#,
                                         #width=W, height=H)
+
+        #ストップするButton
         self.stop_button = Button(self.area_A,
                                         text='stop',
                                         bg=self.area_A_button_color[0],
                                         fg=self.area_A_button_color[1],
                                         command=self.stop)#,
                                         #width=W, height=H)
+
+        #設定画面を開くButton
         self.setting_button = Button(self.area_A,
                                         text='setting',
                                         bg=self.area_A_button_color[0],
                                         fg=self.area_A_button_color[1],
                                         command=self.setting)#,
                                         #width=W, height=H)
+
+        #今の状態を表す部分
         self.state_info = Label(self.area_A, text='state: OFFLINE',
                                         bg=self.area_A_button_color[0],
                                         fg=self.area_A_button_color[1])
-        #self.online_button.grid(column=0, row=0)
-        #self.stop_button.grid(column=1, row=0)
-        #self.setting_button.grid(column=2, row=0)
+        
+        #配置する
         self.online_button.place(x=0, y=0, width=W, height=H)
         self.stop_button.place(x=W, y=0, width=W, height=H)
         self.setting_button.place(x=W * 2, y=0, width=W, height=H)
@@ -76,17 +90,25 @@ class Simple_GUI:
         return
 
     def init_Board_area(self):
+        """
+        Boardを表示するエリア
+        """
+        #1マスの大きさを決める
         W = 40
         H = 40
+        #管理用のlist
         self.sq = []
+        #Othello sfen viewerのコードを流用
         D = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h'}
         for i in range(8):
             rank = []
             for j in range(8):
+                #Buttonである必要はない
                 rank.append(Button(self.Board_area, text=D[j] + str(i + 1),
                                  fg = 'red', bd=2, bg='green',
-                                 relief='ridge'))#, width=W, height=H))
+                                 relief='ridge'))
             self.sq.append(rank)
+        #配置する
         for i in range(8):
             for j in range(8):
                 #self.sq[i][j].grid(column=j, row=i)
@@ -94,21 +116,31 @@ class Simple_GUI:
         return
 
     def init_info_area1(self):
-        #エンジンの情報を表示
+        """
+        エンジンの情報と、手版・持ち時間の情報を表示するエリア
+        """
+        
+        #エンジンの名前を表示する場所
         self.name_area = Label(self.info_area1, text='engine_name',
                                    bg=self.info_area1_bg_color[1], fg=self.info_area1_fg_color[0],
                                    relief='ridge')
+
+        #残り持ち時間を表示する場所
         self.time_area = Label(self.info_area1, text='time: 0',
                                  bg=self.info_area1_bg_color[3], fg=self.info_area1_fg_color[2],
                                  relief='ridge')
+
+        #手番を表示する場所
         self.color_area = Label(self.info_area1, text='color: None',
                                   bg=self.info_area1_bg_color[3], fg=self.info_area1_fg_color[2],
                                  relief='ridge')
+
+        #エンジンメッセージを表示する場所
         self.message_area = Label(self.info_area1, text='engine_message',
                                   bg=self.info_area1_bg_color[2], fg=self.info_area1_fg_color[1],
                                   relief='ridge')
-        #self.name_area.grid(column=0, row=0)
-        #self.message_area.grid(column=0, row=1)
+        
+        #配置
         self.time_area.place(x=0, y=0, width=90, height=30)
         self.color_area.place(x=90, y=0, width=90, height=30)
         self.name_area.place(x=0, y=30, width=180, height=30)
@@ -116,68 +148,107 @@ class Simple_GUI:
         return
 
     def init_info_area2(self):
+        """
+        評価値グラフを表示する場所
+        """
+        #グラフを描画する場所を用意し、真ん中に線を引く
         self.graph_area = Canvas(self.info_area2, bg=self.info_area2_bg)
         self.graph_area.create_rectangle(0, 74, 500, 76, fill='darkblue')
+        #配置
         self.graph_area.place(x=0, y=0, width=500, height=150)
         return
 
     def plot_graph(self, values):
+        """
+        評価値のリストを渡すと、グラフを描いてくれる
+        どんなスケールのグラフでも描ける
+        """
+        #手数を取得
         moves = len(values)
+        #最小値・最大値を取得
         max_value = max(values)
         min_value = min(values)
+        #横幅を決める
         W = (500 / moves)
         for i in range(moves):
+            #高さを計算
             H = int(50 * (values[i] / max([abs(max_value), abs(min_value)])))
+            #描画
             if H >= 75:
                 self.graph_area.create_rectangle((W * i), H, (W * (i + 1)), 75, fill=self.graph_color)
             else:
                 self.graph_area.create_rectangle((W * i), 75, (W * (i + 1)), (75 - H), fill=self.graph_color)
+        #更新
         self.window.update()
         return
 
     def online_play(self):
+        #online_buttonが押されたときに動く関数
         pass
 
     def stop(self):
+        #stop_buttonが押されたときに動く関数
         pass
 
     def setting(self):
+        #setting_buttonが押されたときに動く関数
         pass
 
     def reset_setting(self):
+        #setting_windowのreset_setting_buttonが押されたときに動く関数
         pass
 
     def save_setting(self):
+        #setting_windowのsave_buttonが押されたときに動く関数
         pass
 
     def update_board(self, sfen):
+        """
+        Board_areaをsfenを使って更新する
+        """
         color_d = {'O': 'white', 'X': 'black', '-': 'green'}
         for i in range(8):
             for j in range(8):
+                #色を変える
                 self.sq[i][j].configure(bg=color_d[sfen[(i * 8) + j]])
+        #更新
+        self.window.update()
         return
 
     def make_setting_window(self, change):
+        """
+        setting_windowを作る。
+        setting_windowでは設定を変えられる
+        """
+        #windowを作り、タイトル・サイズを設定する
         self.setting_window = Tk()
         self.setting_window.title('Azisai_Othello_GUI_Setting')
         self.setting_window.geometry('300x300')
+
+        #今の設定をコピー
         self.after_change_setting = change.copy()
-        
+
+        #接続先を設定する場所
         self.text1 = Label(self.setting_window, text='host')
         self.host_input = Text(self.setting_window)
 
+        #対局するエンジンを設定する場所
         self.text2 = Label(self.setting_window, text='engine')
         self.engine_input = Text(self.setting_window)
 
+        #アカウント名を設定する場所
         self.text3 = Label(self.setting_window, text='player_name')
         self.player_name_input = Text(self.setting_window)
 
+        #パスワードを設定する場所
         self.text4 = Label(self.setting_window, text='password')
         self.password_input = Text(self.setting_window)
 
+        #設定をデフォルトに戻すbutton
         self.reset_setting_button = Button(self.setting_window, text='reset',
                                                 bg='red', fg='black', relief='ridge', command=self.reset_setting)
 
+        #設定を保存するbutton
         self.save_button = Button(self.setting_window, text='save', bg='white', fg='black',
                                       relief='ridge', command=self.save_setting)
 
--- main_v1.py
+++ main_v1.py
@@ -10,20 +10,29 @@ import json
 class main(GUI):
     def __init__(self):
         super().__init__()
+        #設定を保存するファイル
         self.setting_file = 'setting.json'
+        #エンジンの型を用意
         self.engine = USI_X_Engine()
+        #設定を読み込む
         self.reset()
 
     def reset(self):
+        #ファイルを開く
         with open(self.setting_file, 'r') as f:
             self.setting = json.load(f)
+        #接続先
         self.host = self.setting['host']
+        #[アカウント名, パスワード]
         self.player_info = self.setting['player_info']
+        #USI-Xエンジン
         self.engine.Engine_path = self.setting['engine']
+        #エンジンの名前を表示するところを更新
         self.name_area.configure(text=self.engine.Engine_path)
         return
 
     def graph_test(self):
+        #グラフ機能のテスト
         import numpy.random as R
         scores = list(range(-30, 60))
         #scores = R.randint(-60, 60, 30)
@@ -31,6 +40,7 @@ class main(GUI):
         return
 
     def reset_setting(self):
+        #設定の初期化
         d = {'host': '127.0.0.1', 'player_info': ['usix_test', '1234'], 'engine': 'random_kun.bat'}
         with open(self.setting_file, 'w') as f:
             json.dump(d, f)
@@ -41,32 +51,44 @@ class main(GUI):
         return
 
     def save_setting(self):
+        #設定の保存
+        #各種値を取得
         self.setting['host'] = ''.join(self.host_input.get('1.0', 'end').splitlines())
         self.setting['engine'] = ''.join(self.engine_input.get('1.0', 'end').splitlines())
         self.setting['player_info'][0] = ''.join(self.player_name_input.get('1.0', 'end').splitlines())
         self.setting['player_info'][1] = ''.join(self.password_input.get('1.0', 'end').splitlines())
+        #ファイルに保存
         with open(self.setting_file, 'w') as f:
             json.dump(self.setting, f)
+        #設定ウィンドウを閉じる
         self.setting_window.destroy()
         return
 
     def online_play(self):
+        #止められるまで続ける
         self.stop = False
         while not self.stop:
+            #状態を更新
             self.state_info.configure(text='state: CONNECT')
+            #ゲームをプレイ
             self.play_game()
             time.sleep(10)
+        #状態を更新
         self.state_info.configure(text='state: OFFLINE')
         return
 
     def stop(self):
+        #「止める」が押されたときに動く関数
         self.stop = True
         return
 
     def setting(self):
+        #「設定」が押されたときに動く関数
         self.make_setting_window(self.setting)
+        return
         
     def update_engine_message(self):
+        #これはいらない
         while self.playing:
             if len(self.engine.engine_message_list) >= 1:
                 self.message_area.configure(text=self.engine.engine_message_list[-1])
@@ -74,82 +96,124 @@ class main(GUI):
         return
 
     def play_game(self):
+        #いろいろ初期化
         board = reversi.Board()
         self.engine.NewGame()
         self.playing = True
         #engine_message_thread = Thread(target=self.update_engine_message)
         #engine_message_thread.start()
 
+        #接続・ログイン・対局待ちを行う
         self.cliant = Cliant(host=self.host)
         self.cliant.login(self.player_info[0], self.player_info[1])
         summary = self.cliant.wait()
         
+        #承諾し、Boardを表示する
         self.cliant.agree()
         self.update_board(board_to_sfen(board, 1))
-        
+
+        #自分の手番を取得
         color = summary['color']
+        #エンジンに送り付けるpositionコマンドの準備
         self.to_engine_message = 'position ' + summary['position']
+        #自分の残り持ち時間を取得
         self.my_time = summary['time']['total']
+        #黒番なら行動
         if color == 'black':
+            #更新
             self.color_area.configure(text='color: Black')
             self.window.update()
+            #positionコマンドをエンジンに送信
             self.engine.command(self.to_engine_message)
+            #goコマンドを用意 & エンジンに送信
             go_message = 'go btime ' + str(self.my_time * 1000) + ' wtime 60000'
-            move = self.engine.command(go_message)
+            self.engine.command(go_message)
+            #bestmoveの取得・エンジンからのメッセージを表示
             move = self.engine.read('bestmove')
             self.message_area.configure(text=self.engine.engine_message_list[-1])
-            
+            #必要な情報を取り出す
             move = move[9] + move[10]
+            #boardに反映
             board.move_from_str(move)
+            #手を送信 & 消費時間を取得
             m, t = self.cliant.send_move(move, color)
+            #持ち時間の管理
             self.my_time -= t
             self.my_time += summary['time']['inc']
+            #positionコマンドを更新
             self.to_engine_message += (' ' + move)
 
         else:
+            #白番では行動しない
+            #更新は行う
             self.color_area.configure(text='color: White')
         
+        #時間を表示 & windowの更新
         self.time_area.configure(text='time: ' + str(self.my_time))
         self.window.update()
-        
+
+        #メインループ・対局が終わるまで
         while True:
+            #windowを更新
             self.update_board(board_to_sfen(board, 1))
+            
             #相手の番
+            #相手の手を取得
             move, t = self.cliant.get_move()
+            #終局したか？
             if move == 'end':
+                #終局したらループから抜ける
                 break
+            #相手の手を反映
             board.move_from_str(move)
             self.to_engine_message +=  (' ' + move)
             #相手の番終わり
-            
+
+            # windowを更新
             self.update_board(board_to_sfen(board, 1))
+
             #自分の番
+            #positionコマンドを送信
             self.engine.command(self.to_engine_message)
+            #goコマンドを準備 & 送信
             if color == 'black':
                 go_message = 'go btime ' + str(self.my_time) + ' wtime 60000'
             else:
                 go_message = 'go btime 60000 wtime ' + str(self.my_time)
             self.engine.command(go_message)
+            #bestmoveを取得 & windowに反映
             move = self.engine.read('bestmove')
             self.message_area.configure(text=self.engine.engine_message_list[-1])
+            #特殊な手の処理を行う
             if 'resign' in move:
                 pass
             if 'pass' in move:
                 pass
+            #必要な情報のみ取得
             move = move[9] + move[10]
+            #boardに反映
             board.move_from_str(move)
+            #手を送信
             m, t = self.cliant.send_move(move, color)
+            #終局したか？
             if m == 'end':
+                #終局したらループから抜ける
                 break
+            #時間の管理
             self.my_time -= t
             self.my_time += summary['time']['inc']
+            #positionコマンドを更新
             self.to_engine_message += (' ' + move)
+            #windowをアップデート
             self.time_area.configure(text='time: ' + str(self.my_time))
             self.window.update()
             #自分の番終わり
+
         self.playing = False
-        engine_message_thread.join()
+        #engine_message_thread.join()
+        #windowを更新
         self.update_board(board_to_sfen(board, 1))
+        #ログアウトを試す
         try:
             self.cliant.logout()
         except:
